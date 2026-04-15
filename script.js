317ecf02e91e7646e5f58f15d32266e24056e6aa
Brynner Jay Antiquesa <imbamystery@gmail.com>
2026-04-15 22:19:33 +0800

    add getHumanChoice() for the user to type 'rock', 'paper' or 'scissors'

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+function getHumanChoice() {
+    let hand = prompt("Please type 'rock', 'paper', 'scissors'");
+    return hand;
+}
